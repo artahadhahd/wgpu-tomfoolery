81e16b11efdd631c2599ab363405d31b9d45fac0
artahadhahd <artaazadegan1@gmail.com>
2023-10-18 16:21:41 +0200

    hello triangle

--- src/lib.rs
+++ src/lib.rs
@@ -18,6 +18,7 @@ struct State {
     window: Window,
     clear_color: wgpu::Color,
     current_key: ScanCode,
+    pipe_line: wgpu::RenderPipeline,
 }
 
 impl State {
@@ -86,6 +87,59 @@ impl State {
         };
         surface.configure(&device, &config);
 
+        let shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
+            label: Some("Triangle"),
+            source: wgpu::ShaderSource::Wgsl(include_str!("shader.wgsl").into()),
+        });
+        let render_pipeline_layout =
+            device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+                label: Some("Render Pipeline Layout"),
+                bind_group_layouts: &[],
+                push_constant_ranges: &[],
+            });
+
+        let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+            label: Some("Render Pipeline"),
+            layout: Some(&render_pipeline_layout),
+            vertex: wgpu::VertexState {
+                module: &shader,
+                entry_point: "vs_main", // 1.
+                buffers: &[],           // 2.
+            },
+            fragment: Some(wgpu::FragmentState {
+                // 3.
+                module: &shader,
+                entry_point: "fs_main",
+                targets: &[Some(wgpu::ColorTargetState {
+                    // 4.
+                    format: config.format,
+                    blend: Some(wgpu::BlendState::REPLACE),
+                    write_mask: wgpu::ColorWrites::ALL,
+                })],
+            }),
+            // continued ...
+            primitive: wgpu::PrimitiveState {
+                topology: wgpu::PrimitiveTopology::TriangleList, // 1.
+                strip_index_format: None,
+                front_face: wgpu::FrontFace::Ccw, // 2.
+                cull_mode: Some(wgpu::Face::Back),
+                // Setting this to anything other than Fill requires Features::NON_FILL_POLYGON_MODE
+                polygon_mode: wgpu::PolygonMode::Fill,
+                // Requires Features::DEPTH_CLIP_CONTROL
+                unclipped_depth: false,
+                // Requires Features::CONSERVATIVE_RASTERIZATION
+                conservative: false,
+            },
+            // continued ...
+            depth_stencil: None, // 1.
+            multisample: wgpu::MultisampleState {
+                count: 1,                         // 2.
+                mask: !0,                         // 3.
+                alpha_to_coverage_enabled: false, // 4.
+            },
+            multiview: None, // 5.
+        });
+
         Self {
             surface,
             device,
@@ -100,6 +154,7 @@ impl State {
                 a: 0_f64,
             },
             current_key: 0,
+            pipe_line: render_pipeline,
         }
     }
 
@@ -138,9 +193,7 @@ impl State {
         }
     }
 
-    fn update(&mut self) {
-        
-    }
+    fn update(&mut self) {}
 
     fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
         let output = self.surface.get_current_texture()?;
@@ -154,19 +207,43 @@ impl State {
                 label: Some("Render Encoder"),
             });
 
-        
-        let render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+        // let render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+        //     label: Some("Render Pass"),
+        //     color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+        //         view: &view,
+        //         resolve_target: None,
+        //         ops: wgpu::Operations {
+        //             load: wgpu::LoadOp::Clear(self.clear_color),
+        //             store: true,
+        //         },
+        //     })],
+        //     depth_stencil_attachment: None,
+        // });
+
+        // render()
+
+        // ...
+
+        // 1.
+        let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
             label: Some("Render Pass"),
-            color_attachments: &[Some(wgpu::RenderPassColorAttachment {
-                view: &view,
-                resolve_target: None,
-                ops: wgpu::Operations {
-                    load: wgpu::LoadOp::Clear(self.clear_color),
-                    store: true,
-                },
-            })],
+            color_attachments: &[
+                // This is what @location(0) in the fragment shader targets
+                Some(wgpu::RenderPassColorAttachment {
+                    view: &view,
+                    resolve_target: None,
+                    ops: wgpu::Operations {
+                        load: wgpu::LoadOp::Clear(self.clear_color),
+                        store: true,
+                    },
+                }),
+            ],
             depth_stencil_attachment: None,
         });
+
+        render_pass.set_pipeline(&self.pipe_line);
+        render_pass.draw(0..3, 0..1);
+
         drop(render_pass);
 
         self.queue.submit(iter::once(encoder.finish()));
@@ -195,7 +272,7 @@ pub async fn run() {
         // Winit prevents sizing with CSS, so we have to set
         // the size manually when on web.
         use winit::dpi::PhysicalSize;
-        window.set_inner_size(PhysicalSize::new(450, 400));
+        window.set_inner_size(PhysicalSize::new(900, 500));
 
         use winit::platform::web::WindowExtWebSys;
         web_sys::window()
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,5 @@
 use first_project::run;
 
 fn main() {
-    cfg_if::cfg_if! {
-        // if #[cfg(debug_assertions)] {
-        //     cfg_if::cfg_if! {
-        //         if #[cfg(target_arch = "wasm32")] {
-        //             println!("Compiling in debug mode");
-        //         }
-        //     }
-        // }
-        if #[cfg(all(debug_assertions, target_arch = "wasm32"))] {
-            println!("Compiled in debug more for wasm32");
-        }
-    }
     pollster::block_on(run());
 }
