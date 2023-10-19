eea676674e28341dfd3b2c81ef79d9aa77a69ac3
artahadhahd <artaazadegan1@gmail.com>
2023-10-19 18:35:33 +0200

    updated utility script

--- compile.py
+++ compile.py
@@ -1,14 +1,39 @@
 #!/usr/bin/env python3
-# quick and dirty utility script
 import os
+import sys
 
-opt = input("""1) Native (Debug)
-2) Web
-3) start local HTTP server
-> """)
-if opt == "1":
-    os.system("cargo run")
-elif opt == "2":
-    os.system("wasm-pack build --target web")
-else:
-    os.system("python3 -m http.server")
+opts = \
+"""
+1) Run [Debug]
+2) WASM
+3) Start local HTTP server
+4) Run [Release]
+5) Build [Debug]
+6) Build [Release]
+"""
+
+opt_to_func: dict[str, str] = {
+    '1' : 'cargo run',
+    '2' : 'wasm-pack build --target web',
+    '3' : 'python3 -m http.server',
+    '4' : 'cargo run --release',
+    '5' : 'cargo build',
+    '6' : 'cargo build --release',
+}
+
+print(opts)
+def main() -> int:
+    opt: str
+    try:
+        opt = input("> ")
+    except EOFError:
+        print()
+        return 1
+    if opt not in opt_to_func.keys():
+        print("Invalid option")
+        return main()
+    os.system(opt_to_func[opt])
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main())
